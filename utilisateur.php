e44fc5bc322cd23ecb89314d7ef1d4a81f790a6f
fatoumatabintoukebe1-star <fatoumatabinetoukebe1@gmail.com>
2026-04-26 17:56:29 +0000

    remplissage classe utilisateur

---
+++ utilisateur.php
@@ -0,0 +1,40 @@
+<?php 
+require_once"Personne.php";
+class Utilisateur extends Personne{
+    private $login;
+    private $motDePasse;
+public function __construct($id, $nom, $email, $login, $motDePasse){
+    parent::__construct($id, $nom, $email);
+
+    $this->login = $login;
+    $this->motDePasse = $motDePasse;
+}
+public function seConnecter(){
+    echo "Utilisateur connecte : " . $this->login;
+}
+public function getLogin() {
+        return $this->login;
+}
+
+public function getMotDePasse() {
+        return $this->motDePasse;
+}
+
+    
+public function setLogin($login) {
+        $this->login = $login;
+}
+
+public function setMotDePasse($motDePasse) {
+        $this->motDePasse = $motDePasse;
+}
+public function afficherUtilisateur() {
+
+        $this->afficherInfos();
+
+        echo "Login : " . $this->login . "<br>";
+}
+    
+}
+
+?>
