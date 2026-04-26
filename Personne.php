b9779dddafa0ebf36b07d4836559d29abb74c91a
fatoumatabintoukebe1-star <fatoumatabinetoukebe1@gmail.com>
2026-04-26 17:14:49 +0000

    creation classe Personne

---
+++ Personne.php
@@ -0,0 +1,37 @@
+<?php 
+class Personne{
+    private $id;
+    private $nom;
+    private $email;
+    
+public function __construct($id, $nom, $email){
+    $this->id = $id;
+    $this->nom = $nom;
+    $this->enail = $email;
+}
+public function getId(){
+    return $this->id;
+}
+public function getNom(){
+    return $this->nom;
+}
+public function getEmail(){
+    return $this->email;
+}
+public function setId($id){
+    $this->id = $id;
+}
+public function setNom($nom){
+    $this->nom = $nom;
+}
+public function setEmail($email){
+    $this->email = $email;
+}
+public function afficherInfos(){
+    echo "Id : " .  $this->id . "<br>";
+    echo "Nom : " . $this->nom . "<br>";
+    echo "Email : " . $this->email . "<br>";
+
+}
+}
+?>
